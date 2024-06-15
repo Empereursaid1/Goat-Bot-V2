0b8fd5b697d3bb7c895a3e6131502a3cd62a5d11
Empereursaid1 <julbertinezoungrana@gmail.com>
2024-06-15 20:05:11 +0000

    Update 1help.js

--- scripts/cmds/1help.js
+++ scripts/cmds/1help.js
@@ -61,8 +61,8 @@ module.exports = {
       pageNotFound: "Trang %1 không tồn tại"
     },
     en: {
-      help: "🥷𝐂𝐌𝐃𝐬 🥷❦ɞ\n\n%1\n✶⊶⊷⊶⊷❍\n 𝐏𝐀𝐆𝐄 [ %2/%3 ]\n➤😇 𝙸 𝚑𝚊𝚟𝚎 𝙰𝚌𝚝𝚞𝚊𝚕𝚕𝚢「 %4 」𝐂𝐌𝐃s \n➤🥷 𝐓𝐘𝐏𝐄: %5help <𝙿𝙰𝙶𝙴> t𝙾 𝚝𝚑𝚎 𝚟𝚒𝚎𝚠 𝚕𝚒𝚜𝚝 𝚘𝚏 𝙲𝚖𝚍𝚜\n➤😇 » 𝐓𝐘𝐏𝐄: %5help 𝚃𝚘 𝚝𝚑𝚎 𝚍𝚎𝚝𝚊𝚒𝚕𝚜 𝚘𝚏 𝚑𝚘𝚠 𝚝𝚘 𝚞𝚜𝚎 𝚝𝚑𝚎 𝙲𝚘𝚖𝚖𝚊𝚖𝚍\n๑۩ ,¸¸,ø¤º°`°⚠︎𖤍⚠︎\n│ %6\n✶⊶⊷⊶⊷𖤍",
-      help2: "%1✶⊶⊷⊶⊷𖤍⊶⊷⊶⊷✶\n》😇𝚃𝚑𝚎 𝚋𝚘𝚝 𝚑𝚊𝚜 〚%2〛 𝐂𝐌𝐃𝚜 𝚝𝚑𝚊𝚝 𝚢𝚘𝚞 𝙲𝚊𝚗 𝚄𝚜𝚎🥷\n》😇⊰⊹ 𝗧𝗬𝗣𝗘: %3help ⊹⊱ 𝚝𝚘 𝚟𝚒𝚎𝚠 𝚍𝚎𝚝𝚊𝚒𝚕𝚜 𝚘𝚏 𝙷𝚘𝚠 𝚝𝚘 𝚞𝚜𝚎 𝚃𝚑𝚊𝚝𝚜 𝙲𝚖𝚍𝚜\n😇 ×º°”`˜”°º× 𝐁𝐨𝐭:\n%4\n☆♬○♩●♪♪●♩○♬☆",
+      help: "💦𝐂𝐌𝐃𝐬 💦❦ɞ\n\n%1\n✶⊶⊷⊶⊷❍\n 𝐏𝐀𝐆𝐄 [ %2/%3 ]\n➤😇 𝙸 𝚑𝚊𝚟𝚎 𝙰𝚌𝚝𝚞𝚊𝚕𝚕𝚢「 %4 」𝐂𝐌𝐃s \n➤🥷 𝐓𝐘𝐏𝐄: %5help <𝙿𝙰𝙶𝙴> t𝙾 𝚝𝚑𝚎 𝚟𝚒𝚎𝚠 𝚕𝚒𝚜𝚝 𝚘𝚏 𝙲𝚖𝚍𝚜\n➤😇 » 𝐓𝐘𝐏𝐄: %5help 𝚃𝚘 𝚝𝚑𝚎 𝚍𝚎𝚝𝚊𝚒𝚕𝚜 𝚘𝚏 𝚑𝚘𝚠 𝚝𝚘 𝚞𝚜𝚎 𝚝𝚑𝚎 𝙲𝚘𝚖𝚖𝚊𝚖𝚍\n๑۩ ,¸¸,ø¤º°`°⚠︎𖤍⚠︎\n│ %6\n✶⊶⊷⊶⊷𖤍",
+      help2: "%1✶⊶⊷⊶⊷𖤍⊶⊷⊶⊷✶\n》😇𝚃𝚑𝚎 𝚋𝚘𝚝 𝚑𝚊𝚜 〚%2〛 𝐂𝐌𝐃𝚜 𝚝𝚑𝚊𝚝 𝚢𝚘𝚞 𝙲𝚊𝚗 𝚄𝚜𝚎💦\n》😇⊰⊹ 𝗧𝗬𝗣𝗘: %3help ⊹⊱ 𝚝𝚘 𝚟𝚒𝚎𝚠 𝚍𝚎𝚝𝚊𝚒𝚕𝚜 𝚘𝚏 𝙷𝚘𝚠 𝚝𝚘 𝚞𝚜𝚎 𝚃𝚑𝚊𝚝𝚜 𝙲𝚖𝚍𝚜\n😇 ×º°”`˜”°º× 𝐁𝐨𝐭:\n%4\n☆♬○♩●♪♪●♩○♬☆",
       commandNotFound: "𝐂𝐌𝐃 \"『%1』\" 𝙳𝚘𝚎𝚜 𝙴𝚡𝚒𝚜𝚝",
       getInfoCommand: "╭── NAME ────𖤍\n %1\n├── INFO\n│ Description: %2\n│ Other names: %3\n│ Other names in your group: %4\n│ Version: %5\n│ Role: %6\n│ Time per command: %7s\n│ Author: %8\n├── Usage\n%9\n├── Notes\n│ The content inside <XXXXX> can be changed\n│ The content inside [a|b|c] is a or b or c\n╰──────𖤍",
       onlyInfo: "╭── INFO ────𖤍\n│ Command name: %1\n│ Description: %2\n│ Other names: %3\n│ Other names in your group: %4\n│ Version: %5\n│ Role: %6\n│ Time per command: %7s\n│ Author: %8\n╰─────────────𖤍",
@@ -128,7 +128,7 @@ module.exports = {
 
         const returnArray = allPage[page - 1] || [];
         const startNumber = (page - 1) * numberOfOnePage + 1;
-        msg += (returnArray || []).reduce((text, item, index) => text += `${index + startNumber}✨${index + startNumber < 10 ? " " : ""}✨.🥷${item.data}🥷\n`, '').slice(0, -1);
+        msg += (returnArray || []).reduce((text, item, index) => text += `${index + startNumber}🍁${index + startNumber < 10 ? " " : ""}🍁.💦${item.data}💦\n`, '').slice(0, -1);
         await message.reply(getLang("help", msg, page, totalPage, commands.size, prefix, doNotDelete));
       }
       else if (sortHelp == "category") {
@@ -150,7 +150,7 @@ module.exports = {
   }
 
   for (const [category, commands] of categoryCommands) {
-    msg += `✰⚔️${category}⚔️✰\n🥷${commands.join(" ")}🥷\n`;
+    msg += `✰⚔️${category}⚔️✰\n🥷${commands.join(" ")}💦\n`;
   }
 
   message.reply(getLang("help2", msg, commands.size, prefix, doNotDelete));
